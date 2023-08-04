2a411a541a7c7c48d872b31f5146142217207514
DDSR <damian@d2600.com>
2023-08-03 22:21:09 -0700

    exercise for lambda & stream course resolved

--- src/main/java/com/test/vz2/planets/PlanetsUtils.java
+++ src/main/java/com/test/vz2/planets/PlanetsUtils.java
@@ -1,23 +1,41 @@
 package com.test.vz2.planets;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlanetsUtils {
 
     public List<Planet> getPlanetsBeginningWithM(List<Planet> planets) {
-        return null;
+        List <Planet> returnList = planets.stream()
+                        .filter(planet -> planet.getName().startsWith("M"))
+                                .collect(Collectors.toList());
+//        planets.stream()
+        return returnList;
     }
 
     public List<Planet> getPlanetsWithRings(List<Planet> planets) {
-        return null;
+        List <Planet> returnList = planets.stream()
+                .filter(planet -> planet.isHasRings())
+                .collect(Collectors.toList());
+//        planets.stream()
+        return returnList;
     }
 
     public List<Planet> getPlanetsWithMoreThanThreeMoons(List<Planet> planets) {
-        return null;
+        List <Planet> returnList = planets.stream()
+                .filter(planet -> planet.getNumberOfMoons() > 3)
+                .collect(Collectors.toList());
+//        planets.stream()
+        return returnList;
+//        return null;
     }
 
     public List<Planet> getPlanetsWithDensityGreaterThanFive(List<Planet> planets) {
-        return null;
+        List <Planet> returnList = planets.stream()
+                .filter(planet -> planet.getDensity() > 5)
+                .collect(Collectors.toList());
+//        planets.stream()
+        return returnList;
     }
 
 }
