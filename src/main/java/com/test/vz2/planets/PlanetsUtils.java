cb2616acc66f49aa5f63e8c5df61ef25b11b09fb
DDSR <damian@d2600.com>
2023-08-03 22:13:26 -0700

    adding last exercise for lambda & stream course

---
+++ src/main/java/com/test/vz2/planets/PlanetsUtils.java
@@ -0,0 +1,23 @@
+package com.test.vz2.planets;
+
+import java.util.List;
+
+public class PlanetsUtils {
+
+    public List<Planet> getPlanetsBeginningWithM(List<Planet> planets) {
+        return null;
+    }
+
+    public List<Planet> getPlanetsWithRings(List<Planet> planets) {
+        return null;
+    }
+
+    public List<Planet> getPlanetsWithMoreThanThreeMoons(List<Planet> planets) {
+        return null;
+    }
+
+    public List<Planet> getPlanetsWithDensityGreaterThanFive(List<Planet> planets) {
+        return null;
+    }
+
+}
